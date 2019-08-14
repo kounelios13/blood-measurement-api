ef1ab7c07f5fed9afec454642ff323a33d16220f
Manos Kounelakis <pkounelios@gmail.com>
2019-08-14 23:09:26 +0300

    get full profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 router.get('/', (req, res, next) => {
-    let user = {
-        ...res.locals.user
-    };
+    let userId = res.locals.user._id;
+    let user = await User.findById(userId).exec();
     let profile = {
         name: user.name,
         surname: user.surname,
